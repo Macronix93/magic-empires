eea1ea8baa8f4cbdaecf00788c294fdabcbce60f
Sylvan Giese <macronix93@gmx.de>
2024-02-23 23:48:40 +0100

    feat: added missing pages

---
+++ buildinglist.php
@@ -0,0 +1,47 @@
+<?php
+global $db_instance, $user;
+require_once("functions.php");
+
+// Check if user is not logged in, and if so, redirect him to login page
+if (!($user->isLoggedIn())) {
+    changeLocation("login.php", 0);
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="de">
+<?php
+include_once("layout/head.html");
+?>
+<body>
+<?php
+include_once("layout/header.php");
+?>
+<div class="content">
+    <div class="content-box">
+        <div class="left-container">
+            <?php
+            include_once("layout/left.php");
+            ?>
+        </div>
+
+        <div class="middle-container">
+            <div class="big-box-container">
+                <div class="big-box-header"><p>Gebäudeliste</p></div>
+                <div class="big-box-content">
+                </div>
+            </div>
+        </div>
+
+        <div class="right-container">
+            <?php
+            include_once("layout/right.php");
+            ?>
+        </div>
+    </div>
+</div>
+<?php
+include_once("layout/footer.php");
+?>
+</body>
+</html>
